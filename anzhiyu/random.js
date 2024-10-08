9b8dd97ff853136cb46e7df398c691ce7e48ac78
fightcheng”git config --global user.email 20009826@qq.comgit config --global user.name fightcheng” <20009826@qq.com>
2024-10-08 13:10:17 +0800

    Site updated: 2024-10-08 13:10:17

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2024/10/08/hello-world/","2024/09/29/对c语言的初步认识/","2024/08/11/搭建博客的心得体会/","2024/09/12/搭建虚拟机的心得体会/","2024/10/08/node_modules/hexo-asset-img/README_zh/","2024/10/08/node_modules/hexo-asset-img/README/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
