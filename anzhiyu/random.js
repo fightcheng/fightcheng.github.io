d6b5da8456a36944a22d5d54eda2472da1c950ae
fightcheng”git config --global user.email 20009826@qq.comgit config --global user.name fightcheng” <20009826@qq.com>
2025-03-16 23:18:21 +0800

    Site updated: 2025-03-16 23:18:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/08/hello-world/","2024/10/12/分析汇编任务/","2024/10/17/动态调试简述/","2024/09/29/对c语言的初步认识/","2024/08/11/搭建博客的心得体会/","2024/10/23/新手esp定律脱壳/","2024/09/12/搭建虚拟机的心得体会/","2024/10/11/汇编代码指令初步学习/","2024/10/08/node_modules/hexo-asset-img/README/","2024/10/08/node_modules/hexo-asset-img/README_zh/","2025/03/15/GHCTF-2025-FishingKit题解/"];function toRandomPost(){
+var posts=["2025/03/15/GHCTF-2025-FishingKit题解/","2024/08/11/搭建博客的心得体会/","2024/10/08/hello-world/","2024/10/12/分析汇编任务/","2024/09/12/搭建虚拟机的心得体会/","2024/10/23/新手esp定律脱壳/","2024/10/17/动态调试简述/","2024/09/29/对c语言的初步认识/","2024/10/11/汇编代码指令初步学习/","2024/10/08/node_modules/hexo-asset-img/README_zh/","2024/10/08/node_modules/hexo-asset-img/README/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
